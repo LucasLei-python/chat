4ef28abaa44d1df4d33530ca1aff69b6bf444d8a
Lucas <826746996@qq.com>
2019-12-24 16:08:28 +0800

    v1.2

--- 交友网/sourcescode/main.py
+++ 交友网/sourcescode/main.py
@@ -3,43 +3,39 @@
 
 
 from flask import Flask, render_template
-from flask import request
+from flask import request,make_response
 from mysql01 import Mysql01
+
 
 app = Flask(__name__)
 
-# my01 = Mysql01()
-# my01.query()
-# my01.close()#执行完毕必须调用close()方法
+my01 = Mysql01()
+
+my01.query("select *from user_info")
+my01.close()  # 执行完毕必须调用close()方法
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return "Hello world"
+    if request.method == "GET":
+        return "Hello world"
     # render_template("../templates/index.html")
 
-
+age=28
 # 需要跳转的页面
 @app.route("/Default", methods=['GET', 'POST'])
 def maintain():
+
     return render_template("Default.html")
 
 
-# @app.route('/signin', methods=['GET'])
-# def signin_form():
-#     return '''<form action="/signin" method="post">
-#               <p><input name="username"></p>
-#               <p><input name="password" type="password"></p>
-#               <p><button type="submit">Sign In</button></p>
-#               </form>'''
-#
-# #从signin页面获取参数值
-# @app.route('/signin', methods=['POST'])
-# def signin():
-#     # 需要从request对象读取表单内容：
-#     if request.form['username'] == 'admin' and request.form['password'] == 'password':
-#         return '<h3>Hello, admin!</h3>'
-#     return '<h3>Bad username or password.</h3>'
+@app.route("/Building", methods=['GET', 'POST'])
+def building():
+    return render_template("Building.html")
+
+@app.route("/chatmain",methods=['GET','POST'])
+def chating():
+    return render_template("chatmain.html")
 
 
 if __name__ == '__main__':
